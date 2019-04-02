68fd9542acd348db68eb62bd9886fea6bbe2d8a1
rouzbehhz <rouzbehhz@gmail.com>
2019-04-02 16:17:09 -0400

    npm module

--- index.js
+++ index.js
@@ -1,20 +1,66 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+export default async function averageColor(img) {
+  if (img === null || img === undefined) {
+    return {
+      hexCode: null,
+      rgbArray: null,
+      rgbObj: null,
+      rgbString: null
+    };
+  }
+  let canvas = document.createElement("canvas"), // Create canvas
+    context = canvas.getContext && canvas.getContext("2d"), // Set canvas context
+    color = { rgbObj: { r: 0, g: 0, b: 0 } }, // Default rgbObj in color
+    skip = 5, // Skip every 5th pixel to sample
+    count = -715, // Init count (-715 is the magical number)
+    counter = -4; // Init counter
 
-const app = express();
+  // Get canvas height and width from image
+  let height = (canvas.height = img.height),
+    width = (canvas.width = img.width);
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+  // If browser doesn't support canvas or if img width/height is 0, throw generic error
+  if (!context || width === 0 || height === 0) {
+    alert("Something went terribly wrong...");
+    return color;
+  }
 
+  // Draw image to canvas
+  context.drawImage(img, 1, 1);
 
-app.use(express.static(`${__dirname}/public`))
+  // Get image data from canvas
+  let data = context.getImageData(1, 1, width, height);
+  data = data.data;
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+  // Sample values based on skip intervals
+  while ((counter += skip * 4) < data.length) {
+    count++;
+    color.rgbObj.r += data[counter];
+    color.rgbObj.g += data[counter + 1];
+    color.rgbObj.b += data[counter + 2];
+  }
 
-const port = process.env.PORT || 3000;
+  // Get average RGB values ( Math.floor() / ~~ ) and set in color Object
+  color.rgbObj.r = ~~(color.rgbObj.r / count);
+  color.rgbObj.g = ~~(color.rgbObj.g / count);
+  color.rgbObj.b = ~~(color.rgbObj.b / count);
 
-app.listen(port, () => {
-  console.log(`App started on ${port}`)
-});
+  // Set RGB as an Array in color Object
+  color.rgbArray = [color.rgbObj.r, color.rgbObj.g, color.rgbObj.b];
+
+  // Set RGB as a String in color Object
+  color.rgbString = `rgb(${color.rgbObj.r},${color.rgbObj.g},${
+    color.rgbObj.b
+  })`;
+
+  // Set HEX code as a String in color Object
+  color.hexCode = color.rgbArray.map(x => {
+    // Convert RGB color values to a Base16 string
+    x = parseInt(x).toString(16);
+    // Add zero if only one character
+    return x.length === 1 ? `0${x}` : x;
+  });
+  color.hexCode = `#${color.hexCode.join("")}`;
+
+  // Export color object
+  return color;
+}
